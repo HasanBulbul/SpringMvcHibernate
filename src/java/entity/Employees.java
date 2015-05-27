9a3e2320cdb199d40c5e60654308e29a11072844
Hasan Bülbül <hsn.blbl95@gmail.com>
2015-05-27 09:37:53 +0300

    Update domain model
    Override hash and equals methods add project. Implements java.io.Serializable  package remove.

--- src/java/entity/Employees.java
+++ src/java/entity/Employees.java
@@ -13,7 +13,7 @@ import javax.persistence.Table;
  */
 @Entity
 @Table(name="Employees")
-public class Employees  implements java.io.Serializable {
+public class Employees {
 
 
      private int id;
