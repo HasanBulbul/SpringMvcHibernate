6f68fa28738e828e6739452331590649f46f77ef
Hasan Bülbül <hsn.blbl95@gmail.com>
2015-05-27 10:03:11 +0300

    Update EmployeeModel.java
    Remove comment in page.

--- src/java/model/EmployeeModel.java
+++ src/java/model/EmployeeModel.java
@@ -1,23 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package model;
 
 import entity.*;
 import java.util.*;
 import org.hibernate.*;
 
-/**
- *
- * @author Hasan
- */
 public class EmployeeModel {
-    
-    
-    //Crud methods---------------------------
     
     public List<Employees> getAll(){
         
