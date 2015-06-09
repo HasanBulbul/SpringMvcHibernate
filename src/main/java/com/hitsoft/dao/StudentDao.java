4a347f0e897d3dcb49d4771f8a4549690443e34d
Hasan Bübül <hsn.blbl95@gmail.com>
2015-06-09 15:19:18 +0300

    Refactoring
    I update the web application and  moved to maven

---
+++ src/main/java/com/hitsoft/dao/StudentDao.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.hitsoft.dao;
+
+import com.hitsoft.model.Student;
+import java.util.List;
+
+/**
+ *
+ * @author Hasan
+ */
+public interface StudentDao {
+    
+    public void create(Student s);
+    public void edit(Student s);
+    public void remove(Student s);
+    public Student getStd(int id);
+    public List getAll();
+    
+}
---
+++ src/main/java/com/hitsoft/model/Student.java
@@ -0,0 +1,115 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.hitsoft.model;
+
+import java.util.Objects;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author Hasan
+ */
+@Entity
+@Table(name="Student"
+    ,schema="dbo"
+    ,catalog="Spring1"
+)
+public class Student {
+    
+    private int id;
+    private String firstname;
+    private String lastname;
+    private int yearLevel;
+
+    public Student (){}
+    
+    public Student(int id, String firstname, String lastname, int yearLevel) {
+        this.id = id;
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.yearLevel = yearLevel;
+    }
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name="id", unique=true, nullable=false)
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Column(name="firstname")
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    @Column(name="lastname")
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    @Column(name="yearLevel")
+    public int getYearLevel() {
+        return yearLevel;
+    }
+
+    public void setYearLevel(int yearLevel) {
+        this.yearLevel = yearLevel;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 79 * hash + this.id;
+        hash = 79 * hash + Objects.hashCode(this.firstname);
+        hash = 79 * hash + Objects.hashCode(this.lastname);
+        hash = 79 * hash + this.yearLevel;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Student other = (Student) obj;
+        if (this.id != other.id) {
+            return false;
+        }
+        if (!Objects.equals(this.firstname, other.firstname)) {
+            return false;
+        }
+        if (!Objects.equals(this.lastname, other.lastname)) {
+            return false;
+        }
+        if (this.yearLevel != other.yearLevel) {
+            return false;
+        }
+        return true;
+    }
+    
+    
+}
